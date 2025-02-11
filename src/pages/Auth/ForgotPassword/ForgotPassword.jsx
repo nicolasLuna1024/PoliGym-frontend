3f44d10196da4fb8887eced43ab66f7fc65440e7
nicolasLuna1024 <nicolas.luna@epn.edu.ec>
2025-02-11 00:08:26 -0500

    Corrigiendo importación de recover.css

--- src/pages/Auth/ForgotPassword/ForgotPassword.jsx
+++ src/pages/Auth/ForgotPassword/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Recover.css";
+import "./recover.css";
 import { toast, ToastContainer } from "react-toastify";
 
 const ForgotPassword = () => {
